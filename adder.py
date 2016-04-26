8da20b2b5aeb96ccb78b42099883de4dd5433b3e
lsw8075 <lsw8075@snu.ac.kr>
2016-04-26 17:01:03 +0900

    사칙연산 등,  예외처리 구현  Please enter the commit message for your changes. Lines starting

--- adder.py
+++ adder.py
@@ -1,5 +1,18 @@
 
-a = int(input("Input 1st number:"))
-b = int (input("Input 2nd number:"))
-print(a, "+", b, "=", a+b)
+while True: 
+
+	a = int(input("Input 1st number:"))
+	b = int(input("Input 2nd number:"))
+ 	
+	if a == 0 and b == 0:
+		break
+	
+	print(a, "+", b, "=", a+b)
+	print(a, "-", b, "=", a-b)
+	print(a, "*", b, "=", a*b)
+	if b != 0 :
+		print(a, "/", b, "=", a/b)
+		print(a, "%", b, "=", a%b)
+	print(a, "**", b, "=", a**b)
+
 
